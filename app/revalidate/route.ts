5e7167530db27a3a1d964d8d699896fa34b67345
Jason Lengstorf <jason@lengstorf.com>
2024-05-23 10:48:49 -0700

    fix: force route revalidation to be dynamic

--- app/revalidate/route.ts
+++ app/revalidate/route.ts
@@ -1,6 +1,9 @@
 import { revalidatePath } from 'next/cache';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(_request: Request) {
 	revalidatePath('/');
+
 	return new Response('ok');
 }
